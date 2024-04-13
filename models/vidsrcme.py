98c2b2f8c57a4486083eaa554831f9e54cede6c7
manoj4000 <74961587+manoj4000@users.noreply.github.com>
2024-04-13 13:16:36 +0530

    Update vidsrcme.py

--- models/vidsrcme.py
+++ models/vidsrcme.py
@@ -1,80 +1,56 @@
 import asyncio
-from typing import List, Optional
-from fastapi import HTTPException
+from bs4 import BeautifulSoup
+from . import vidsrcpro,superembed
+from .utils import fetch
+SOURCES = ["VidSrc PRO","Superembed"]
+async def get_source(hash:str,url:str):
+    SOURCE_REQUEST = await fetch(f"https://vidsrc.stream/rcp/{hash}",headers={"Referer": url})
+    try:
+        _html = BeautifulSoup(SOURCE_REQUEST.text, "html.parser")
+        _encoded = _html.find("div", {"id": "hidden"}).get("data-h") if _html.find("div", {"id": "hidden"}) else None
 
+        if not _encoded:
+            return {"stream":None,"subtitle":[]}
+        
+        _seed = _html.find("body").get("data-i")
+        encoded_buffer = bytes.fromhex(_encoded);decoded = ""
+        for i in range(len(encoded_buffer)):decoded += chr(encoded_buffer[i] ^ ord(_seed[i % len(_seed)]))
+        decoded_url = f"https:{decoded}" if decoded.startswith("//") else decoded
+        
+        response = await fetch(decoded_url,redirects=False, headers={"Referer": f"https://vidsrc.stream/rcp/{hash}"})
+        location = response.headers.get("Location")
+        if location is None:
+            return {"stream":None,"subtitle":[]}
+        if "vidsrc.stream" in location:
+            return await vidsrcpro.handle(location,hash,_seed)
+        if "multiembed.mov" in location:
+            return await superembed.handle(location,hash,_seed)
+    except:
+        return {"stream":None,"subtitle":[]}
 
-from vidsrcpro import VidsrcStreamExtractor
-from utils import Utilities
+async def get_stream(hash:str,url:str,SOURCE_NAME:str):
+    if True:
+        RESULT = {}
+        RESULT['name'] = SOURCE_NAME
+        RESULT['data'] = await get_source(hash,url)
+        return RESULT
+    else:
+        return {"name":SOURCE_NAME,"source":'',"subtitle":[]}
 
-class VidsrcMeExtractor:
-    BASE_URL = "https://vidsrc.net" # vidsrc.me / vidsrc.in / vidsrc.pm / vidsrc.xyz / vidsrc.net
-    RCP_URL = "https://vidsrc.stream/rcp"
+async def get(dbid,s=None,e=None,l='eng'):
+    provider = "imdb" if ("tt" in dbid) else "tmdb"
+    media = 'tv' if s is not None and e is not None else "movie"
+    language = l
 
-    def __init__(self, **kwargs) -> None:
-        self.fetch_subtitles = kwargs.get("fetch_subtitles")
+    # MAKE API REQUEST TO GET ID(hash)
+    id_url = f"https://vidsrc.me/embed/{dbid}" + (f"/{s}-{e}" if s and e else '')
+    id_request = await fetch(id_url)
+    _html = BeautifulSoup(id_request.text, "html.parser")
+    SOURCE_RESULTS = [{"name": attr.text, "hash": attr.get("data-hash")} for attr in _html.find_all("div", {"class": "server"}) if attr.text in SOURCES]
 
-    async def get_sources(self, url: str) -> dict:
-        req = await asyncio.get_event_loop().run_in_executor(None, requests.get, url)
-
-        if req.status_code != 200:
-            
-            return {}
-
-        soup = BeautifulSoup(req.text, "html.parser")
-        return {source.text: source.get("data-hash") for source in soup.find_all("div", {"class", "server"}) if source.text and source.get("data-hash")}
-
-    async def get_source(self, hash: str, referrer: str) -> dict:
-        url = f"{self.RCP_URL}/{hash}"
-     
-
-        req = await asyncio.get_event_loop().run_in_executor(None, requests.get, url, {"Referer": referrer})
-        if req.status_code != 200:
-          
-            return {"stream": None, "subtitle": []}
-
-        soup = BeautifulSoup(req.text, "html.parser")
-        encoded = soup.find("div", {"id": "hidden"}).get("data-h")
-        seed = soup.find("body").get("data-i") # this is just the imdb id - the "tt" lol
-
-        source = Utilities.decode_src(encoded, seed)
-        if source.startswith("//"):
-            source = f"https:{source}"
-
-        source_url = await self.get_source_url(source, url)
-        if not source_url:
-
-            return {"stream": None, "subtitle": []}
-
-        final_source_url = await self.get_source_url(source_url, url)
-        if "vidsrc.stream" in final_source_url:
-           
-
-            extractor = VidsrcStreamExtractor()
-            return await extractor.resolve_source(url=source_url, referrer=url)
-
-       
-
-        return {"stream": None, "subtitle": []}
-
-    async def get_source_url(self, url: str, referrer: str) -> str:
-       
-        req = await asyncio.get_event_loop().run_in_executor(None, requests.get, url, {"Referer": referrer})
-        if req.status_code != 302:
-            print(f"[VidSrcExtractor] Couldnt find redirect for \"{url}\", status code: {req.status_code}")
-            return None
-
-        return req.headers.get("location")
-
-async def vidsrcmeget(dbid: str, s: Optional[int] = None, e: Optional[int] = None, l: str = 'eng') -> List[dict]:
-    vse = VidsrcMeExtractor(
-        fetch_subtitles=True
+    # REQUEST THE SOURCE
+    SOURCE_STREAMS = await asyncio.gather(
+        *[get_stream(R.get('hash'),id_url,R.get('name')) for R in SOURCE_RESULTS]
     )
-
-    sources = []
-    source_names = ["VidSrc PRO"]
-
-    for source_name in source_names:
-        source_data = await vse.get_source(dbid, f"https://vidsrc.net/embed/{dbid}")
-        sources.append({"name": source_name, "data": source_data})
-
-    return sources
+    
+    return SOURCE_STREAMS
